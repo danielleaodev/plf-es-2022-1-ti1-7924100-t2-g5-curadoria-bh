8af7551bc310aa0c2a6b1a54003ed010971c35d1
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-05-20 16:37:37 -0300

    feat: salva dados do formulario em um objeto"

--- codigo/places-owners-registration/ownerForms.js
+++ codigo/places-owners-registration/ownerForms.js
@@ -1,20 +1,34 @@
 // todo gerar o jsonOwner
 // todo salvar no localStorage o jsonOwner
 // todo modularizar para conseguir criar testes unitários
+// todo criar validações para os campos
+
+function getRadioValue() {
+	const radiosInput = document.getElementsByName("gender");
+	const checkedRadio = Array.from(radiosInput).find((radio) => radio.checked);
+
+	if (checkedRadio === undefined) {
+		return "";
+	}
+	return checkedRadio.value;
+}
 
 window.onload = () => {
 	const nameInput = document.getElementById("name-input");
 	const emailInput = document.getElementById("email-input");
 	const cpfInput = document.getElementById("cpf-input");
 	const passwordInput = document.getElementById("password-input");
-	const radiosInput = document.getElementsByName("gender");
 	const submitBtn = document.getElementById("submit-btn");
 
 	submitBtn.onclick = () => {
 		const jsonOwner = {
 			name: nameInput.value,
+			email: emailInput.value,
+			cpf: cpfInput.value,
+			password: passwordInput.value,
+			gender: getRadioValue(),
 		};
-		console.log(Array.from(radiosInput).find((radio) => radio.checked).value); // retorna o valor do radio selecionado
+
 		console.log(jsonOwner);
 	};
 };
