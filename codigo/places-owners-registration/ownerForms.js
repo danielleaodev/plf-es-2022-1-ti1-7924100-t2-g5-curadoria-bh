ab1e0bbc2f9d0361a634baa4898d28f191247d81
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-05-20 16:11:33 -0300

    feat: recupera todos os inputs em javascript

--- codigo/places-owners-registration/ownerForms.js
+++ codigo/places-owners-registration/ownerForms.js
@@ -4,13 +4,17 @@
 
 window.onload = () => {
 	const nameInput = document.getElementById("name-input");
+	const emailInput = document.getElementById("email-input");
+	const cpfInput = document.getElementById("cpf-input");
+	const passwordInput = document.getElementById("password-input");
+	const radiosInput = document.getElementsByName("gender");
 	const submitBtn = document.getElementById("submit-btn");
 
 	submitBtn.onclick = () => {
 		const jsonOwner = {
 			name: nameInput.value,
 		};
-
+		console.log(Array.from(radiosInput).find((radio) => radio.checked).value); // retorna o valor do radio selecionado
 		console.log(jsonOwner);
 	};
 };
